d8ab526ec715ac91842a5aa26ae308e9c860fe7f
David zhang <d.ydzdavid@yahoo.com>
2018-12-05 21:54:16 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["snickers", "grand", "kitkat", "skittles"];
+chocolateBars.push("pocky");
+
+function addElementToBeginningOfArray (chocolateBars) {
+  return var newbar = chocolateBars.push("choco")
+}
+
